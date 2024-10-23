3f9984a5767997337949f8146fc41a4d2f0c6d98
Abhay Kashyap <abhaykash12@gmail.com>
2024-10-23 00:09:03 -0700

    Added more tasks to invoke

--- tasks.py
+++ tasks.py
@@ -8,18 +8,18 @@ REQUIREMENTS_DIR = BASE_DIR / "requirements"
 
 @task
 def update(ctx: Context, upgrade: bool = False, hashes: bool = False) -> None:
-    common_args = "-q --allow-unsafe --resolver=backtracking --strip-extras"
-    common_args += " --upgrade" if upgrade else ""
-    common_args += " --generate-hashes" if hashes else ""
+    args = "-q --allow-unsafe --resolver=backtracking --strip-extras"
+    args += " --upgrade" if upgrade else ""
+    args += " --generate-hashes" if hashes else ""
 
     with ctx.cd(REQUIREMENTS_DIR):
         ctx.run(
-            f"pip-compile {common_args} 'requirements.in'",
+            f"pip-compile {args} 'requirements.in'",
             pty=True,
             echo=True,
         )
         ctx.run(
-            f"pip-compile {common_args} 'requirements-dev.in'",
+            f"pip-compile {args} 'requirements-dev.in'",
             pty=True,
             echo=True,
         )
@@ -32,8 +32,48 @@ def install(ctx: Context, dev: bool = False) -> None:
         update(ctx)
 
     command = "pip-sync requirements.txt"
-    if dev:
-        command += " requirements-dev.txt"
+    command += " requirements-dev.txt" if dev else ""
 
     with ctx.cd(REQUIREMENTS_DIR):
         ctx.run(command, pty=True, echo=True)
+
+
+@task
+def build(ctx: Context) -> None:
+    with ctx.cd(BASE_DIR):
+        image_name = "us-west1-docker.pkg.dev/<project_id>/<repo>/<fapi-starter>"
+        ctx.run(
+            f"docker build --target api --tag {image_name} .",
+            pty=True,
+            echo=True,
+        )
+
+
+@task
+def deploy(ctx: Context) -> None:
+    with ctx.cd(BASE_DIR):
+        image_name = "us-west1-docker.pkg.dev/<project_id>/<repo>/<fapi-starter>"
+        ctx.run(
+            f"docker push {image_name}",
+            pty=True,
+            echo=True,
+        )
+        ctx.run(
+            f"gcloud run deploy <fapi-starter> --region us-west1 --image {image_name}",
+            pty=True,
+            echo=True,
+        )
+
+
+@task
+def gitprep(ctx: Context) -> None:
+    with ctx.cd(BASE_DIR):
+        ctx.run("git add .", pty=True, echo=True)
+        ctx.run("pre-commit run", pty=True, echo=True)
+
+
+@task
+def gitpush(ctx: Context, message: str) -> None:
+    with ctx.cd(BASE_DIR):
+        ctx.run(f'git commit -am "{message}"', pty=True, echo=True)
+        ctx.run("git push ", pty=True, echo=True)
